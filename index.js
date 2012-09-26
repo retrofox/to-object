f3b71d563619d596bdcc16c0134db6f5ce2a592a
Damian Suarez <rdsuarez@gmail.com>
2012-09-26 10:34:27 -0300

    first aprox

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+/**
+ * Module dependencies.
+ */
+
+
+/**
+ * Expose `Dropdown`.
+ */
+
+module.exports = arrayToObject;
+
+/**
+ * arrayToObject
+ *
+ * @param {Array} array array to convert
+ * @param {String} key (optional)
+ * @param {Boolean} removeItemKey remove key item (default false)
+ * @param {Function} fn modify function (optional)
+ * @return {Object}
+ */
+
+function arrayToObject(array, key, removeKeyItem, fn) {
+  var obj = {};
+  fn = 'function' == typeof removeKeyItem ? removeKeyItem : fn;
+  removeKeyItem = 'function' == typeof removeKeyItem ? false : removeKeyItem;
+  fn = fn || function(k){ return k; };
+
+  for (var i = 0; i < array.length; i++) {
+    var prp = fn(array[i][key] || i);
+    obj[prp] = array[i];
+    if (removeKeyItem) {
+      delete obj[prp][key];
+    }
+  }
+  return obj;
+}
